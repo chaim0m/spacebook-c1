e493f51e49344e1fb78d8bfa3263bf714504db16
Chaim Finson <chaim0m@hotmail.com>
2017-12-31 10:43:15 +0200

    crazy new branch

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-var counter=0;
-var postArray = [];
+var counter = 0; // id generator
+var postArray = []; //storage location of post object
+var postObject = {};//post individual object
 
-function createPost(){
-  var postObject = {
+function createPost() {
+  postObject = {
     text: $('#post-name').val(),
     id: counter
   };
-  counter+=1;
-  console.log(postObject.text);
+  counter++;
+};
+$('.add-post').on('click', createPost); //stores the post object (on click) in postArray
+
+function storePost() {
   postArray.push(postObject);
-  console.log(postArray);
-  $('.row').append('<p class="post" data-id=postObject.id"><button type="button" class="remove">REMOVE</button>'+postObject.text+'</p>');
+};
+
+function renderPost() { //renders posts to page (on click)
+  counter += 1;
+  $('.row').append('<p class="post" data-id='+postObject.id+'> <button type="button" class="remove">REMOVE</button>'+postObject.text'</p>');
 };
 // console.log(post.postArray);
 
-$('.add-post').on('click',createPost);
+fuction onAddPostClick(){
+  createPost();
+  storePost();
+  renderPost();
+}
+$('.add-post').on('click', onAddPostClick);
+
+//my new branch
